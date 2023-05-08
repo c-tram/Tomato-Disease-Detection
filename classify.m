de07ab209f51417c40108447e359f32ec9292c28
Cole Trammell <117682156+c-tram@users.noreply.github.com>
2023-05-07 22:00:12 -0500

    update

--- classify.m
+++ classify.m
@@ -1,4 +1,4 @@
-% Load and prepare your image dataset
+% Load image datastore and initialize training/validation sets
 pathToImages = imageDatastore('./', 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
 [trainingSet, validationSet] = splitEachLabel(pathToImages, 0.8, 'randomized');
 
@@ -9,16 +9,11 @@ features = {'Area', 'Perimeter', 'Eccentricity', 'ConvexArea', 'Solidity', 'Eule
 trainingFeatures = zeros(length(trainingSet.Files), length(features));
 for i = 1:length(trainingSet.Files)
     img = imread(trainingSet.Files{i});
-    img = imresize(img, targetSize);
-    img = histeq(img); % apply histogram equalization to enhance contrast
-    I = rgb2gray(img);
-    I2 = imadjust(I);
-    I3 = imfilter(I2, fspecial('sobel'));
-    I4 = imopen(I3, strel('disk', 2));
-    mask = imbinarize(I4, graythresh(I4));
-    mask = imfill(mask, 'holes');
-    mask = imerode(mask, strel('disk', 2));
-    mask = imdilate(mask, strel('disk', 2));
+
+    img = imresize(img,targetSize);
+
+    % Do feature extraction here
+    
     stats = regionprops('table',mask, features);
     trainingFeatures(i,:) = mean(stats{:,:}, 'omitnan');
 end
@@ -30,18 +25,14 @@ ourClassifier = fitcecoc(trainingFeatures, trainingLabels);
 % Evaluate our classifier
 validationFeatures = zeros(length(validationSet.Files), length(features));
 for i = 1:length(validationSet.Files)
-    img = imresize(img, targetSize);
-    img = histeq(img); % apply histogram equalization to enhance contrast
-    I = rgb2gray(img);
-    I2 = imadjust(I);
-    I3 = imfilter(I2, fspecial('sobel'));
-    I4 = imopen(I3, strel('disk', 2));
-    mask = imbinarize(I4, graythresh(I4));
-    mask = imfill(mask, 'holes');
-    mask = imerode(mask, strel('disk', 2));
-    mask = imdilate(mask, strel('disk', 2));
-    stats = regionprops('table',mask, features);
-    validationFeatures(i,:) = mean(stats{:,:}, 'omitnan');
+   img = imread(validationSet.Files{i});
+
+   img = imresize(img,targetSize);
+
+    % Do feature extraction here
+    
+   stats = regionprops('table',mask, features);
+   validationFeatures(i,:) = mean(stats{:,:}, 'omitnan');
 end
 validationLabels = validationSet.Labels;
 predictedLabels = predict(ourClassifier, validationFeatures);
--- detect.m
+++ detect.m
@@ -1,33 +1,28 @@
-% Import images of sick tomatoes
+% Import images of tomatoes
 images = imageDatastore('./', 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
 
-targetSize = [227 227];
+targetSize = [100 100];
+
+features = {'Area', 'Perimeter', 'Eccentricity', 'ConvexArea', 'Solidity', 'EulerNumber'};
 % Loop, iterate through images to determine if sick.
 for i = 1:numel(images.Files)
 
 % Load a new image to classify
 newImg = readimage(images,i);
+
 img = imresize(newImg, targetSize);
-    img = histeq(img); % apply histogram equalization to enhance contrast
-    I = rgb2gray(img);
-    I2 = imadjust(I);
-    I3 = imfilter(I2, fspecial('sobel'));
-    I4 = imopen(I3, strel('disk', 2));
-    mask = imbinarize(I4, graythresh(I4));
-    mask = imfill(mask, 'holes');
-    mask = imerode(mask, strel('disk', 2));
-    mask = imdilate(mask, strel('disk', 2));
-    stats = regionprops('table',mask, features);
-stats = regionprops('table', mask, {'Area', 'Perimeter', 'Eccentricity', 'ConvexArea', 'Solidity', 'EulerNumber'});
-newFeatures = mean(stats{:,:});
-% newFeaturesFilled = fillmissing(newFeatures,'next');
+
+% do feature extraction here
+    
+stats = regionprops('table',mask, features);
+features = mean(stats{:,:});
 
 % Use the classifier to predict the label of the new image
 load('Classifier.mat');
-if any(isnan(newFeatures(:)))
-    fprintf('missing features');
+if any(isnan(features(:)))
+    %%do nothing
 else
-    predictedLabel = predict(ourClassifier, newFeatures);
+    predictedLabel = predict(ourClassifier, features);
     % Display results in terminal
     fileName = images.Files;
     fprintf('Image %s:  -> ', fileName{i});
